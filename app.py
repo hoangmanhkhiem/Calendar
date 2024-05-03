99cd1a40f95c73d28d24189eac42221f17e30483
Hoang Manh Khiem <khiemhm04@gmail.com>
2024-05-03 07:06:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,22 @@ import src.exce_file as exce_file
 app = Flask(__name__)
 
 
+# khai báo một route khi trug cập web thì hàm 
+# index() được gọi và home.html được render ra # đàu tiền
 @app.route('/')
 def index():
     return render_template('home.html')
 
 
+# Upload file từ người dùng
 @app.route('/up')
 def up():
     return render_template('import_xls.html')
 
 
+# cho phép người dùng tải lên một tệp Excel 
+# thông qua trang web sau đó gọi đến hàm exce
+# để xử lí dữ liệu
 @app.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
@@ -31,6 +37,8 @@ def upload():
     return render_template('import_xls.html')
 
 
+# người dùng có thể lấy dữ liệu từ một tệp JSON # có tên là 'data.json' từ thư mục 'upload'
+# trên máy chủ. 
 @app.route('/upload/data.json', methods=['GET'])
 def get_data():
     return send_from_directory('upload', 'data.json')
