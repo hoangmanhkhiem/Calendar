1a9cff1df883ea92fe1a5ada9463ea7cab75fd5a
Hoang Manh Khiem <khiemhm04@gmail.com>
2024-05-04 15:59:40 +0700

    Update exce_file.py

--- src/exce_file.py
+++ src/exce_file.py
@@ -1,98 +1,86 @@
 import os
+from os import devnull
 from datetime import datetime
 import xlrd
 import json
 
 data_list = []
 
-def Lop_Hoc_Phan(nName, sheet):
-    parts = nName.split('-')
-    name = parts[0].strip()
-    lophp = ''
-    qt_part = next((part for part in parts if 'QT' in part), None)
-    if qt_part:
-        qt_index = qt_part.index('QT')
-        lophp = qt_part[qt_index:]
-        lophp = lophp[:lophp.index(')')]
-    return name, lophp
 
-
-def Tiet_Hoc(nName, sheet):
-    if nName == '1->3':
+def Tiet_Hoc(nName):
+    if nName == '1,2,3':
         return 1
-    if nName == '4->6':
+    if nName == '4,5,6':
         return 2
-    if nName == '7->9':
+    if nName == '7,8,9':
         return 3
-    if nName == '10->12':
+    if nName == '10,11,12':
         return 4
 
 
 def exce_time(date_str):
-    date_str = date_str.split("-")
+    date_str = date_str.replace('Từ', '')
+    date_str = date_str.replace(':', '')
+    date_str = date_str.split('đến')
     if len(date_str) == 1:
-        return None, None
+        return date_str
     start_date_str = date_str[0]
     end_date_str = date_str[1]
-    start_date = datetime.strptime(start_date_str.strip(), "%d/%m")
+    start_date = datetime.strptime(start_date_str.strip(), "%d/%m/%Y")
     end_date = datetime.strptime(end_date_str.strip(), "%d/%m/%Y")
-    year = end_date.year
-    if start_date.month > end_date.month:
-        year -= 1
-    start_date = start_date.replace(year=year)
+    start_date = start_date.strftime("%Y-%m-%d %H:%M:%S")
+    end_date = end_date.strftime("%Y-%m-%d %H:%M:%S")
+    start_date = str(start_date)
+    end_date = str(end_date)
     return start_date, end_date
 
 
+
+
+
 def exce_info(sheet):
-    lop_hoc_phan_1 = ''
-    tenlop_1 = ''
     start_date = ''
     end_date = ''
-    for row in range(4, sheet.nrows):
-        tenlop, lophocphan = Lop_Hoc_Phan(sheet.cell_value(row, 3), sheet)
-        if lophocphan == '':
-            lophocphan = lop_hoc_phan_1
-            tenlop = tenlop_1
+    for row in range(10, sheet.nrows):
+        ten_mon_hoc = sheet.cell_value(row, 3)
+        lop_hoc_phan = sheet.cell_value(row, 5)
+
+        if ten_mon_hoc == '' and lop_hoc_phan == '':
+            break
+        if ten_mon_hoc == '':
+            ten_mon_hoc = ten_mon_hoc_1
         else:
-            lop_hoc_phan_1 = lophocphan
-            tenlop_1 = tenlop
+            ten_mon_hoc_1 = ten_mon_hoc
 
-        thu = sheet.cell_value(row, 9)
+        thong_tin_ngay_diadiem = sheet.cell_value(row, 7)
 
-        tiet_hoc = Tiet_Hoc(sheet.cell_value(row, 10), sheet)
-
-        phonghoc = sheet.cell_value(row, 11)
-
-        start_time, end_time = exce_time(sheet.cell_value(row, 12))
-        if start_time is not None:
-            start_date = start_time
-        else:
-            start_time = start_date
-        if end_time is not None:
-            end_date = end_time
-        else:
-            end_time = end_date
-
-        start_time = start_time.strftime("%Y-%m-%d %H:%M:%S")
-        end_time = end_time.strftime("%Y-%m-%d %H:%M:%S")
-        start_time = str(start_time)
-        end_time = str(end_time)
-
-        if thu == '':
-            continue
-        data_list.append({
-            'LopHocPhan': lophocphan,
-            'TenLop': tenlop,
-            'Thu': thu,
-            'Ca': tiet_hoc,
-            'PhongHoc': phonghoc,
-            'ThoiGianBatDau': start_time,
-            'ThoiGianKetThuc': end_time
-        })
-
+        nName = thong_tin_ngay_diadiem.split("\n")
+        for i in range(0, len(nName)):
+            if 'Từ' in nName[i]:
+                start_date,end_date = exce_time(nName[i])
+            else:
+                    thong_tin = nName[i].split(" ")
+                    thu = thong_tin[2]
+                    tiet_hoc = thong_tin[4]
+                    tiet_hoc = Tiet_Hoc(tiet_hoc)
+                    if thong_tin[6] == 'Ngoài':
+                        phonghoc = thong_tin[6] + " " + thong_tin[7] + " " + thong_tin[8]
+                    elif thong_tin[6] == 'Phòng':
+                        phonghoc = 'Online'
+                    else:
+                        phonghoc = thong_tin[6]
+                    data_list.append({
+                        'LopHocPhan': lop_hoc_phan,
+                        'TenLop': ten_mon_hoc,
+                        'Thu': thu,
+                        'Ca': tiet_hoc,
+                        'PhongHoc': phonghoc,
+                        'ThoiGianBatDau': start_date,
+                        'ThoiGianKetThuc': end_date
+                    })
 
 def exce_student(nName):
-    workbook = xlrd.open_workbook(nName)
+    workbook = xlrd.open_workbook(nName,logfile=open(devnull, 'w'))
     sheet = workbook.sheet_by_index(0)
     exce_info(sheet)
     with open('upload/data.json', 'w', encoding='utf8') as json_file:
